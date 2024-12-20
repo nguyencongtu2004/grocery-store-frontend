8fd423a5c9e9176faf0ed1f7c713cf5f2ea30c26
Tú <22521591@gm.uit.edu.vn>
2024-12-20 16:37:12 +0700

    Update Logo and loader to use dynamic navigation routes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,6 +3,7 @@ import { cn } from "../ultis/ultis";
 import PropTypes from "prop-types";
 import Row from "./layout/Row.jsx";
 import { Link } from "react-router-dom";
+import { navigation } from "../constants/navigation.js";
 
 Logo.propTypes = {
   className: PropTypes.string,
@@ -10,7 +11,7 @@ Logo.propTypes = {
 
 export default function Logo({ className }) {
   return (
-    <Link to="/report">
+    <Link to={navigation[0].route}>
       <Row className={cn("items-center gap-2", className)}>
         <img className="w-16 h-16" src={logo} alt="logo" />
         <div className="text-center text-[#42b28f] text-4xl font-semibold leading-normal">
--- src/ultis/loader.js
+++ src/ultis/loader.js
@@ -1,11 +1,12 @@
 import { redirect } from "react-router-dom";
+import { navigation } from "../constants/navigation";
 
 export async function rootLoader({ request }) {
   const url = new URL(request.url);
   console.log(url.pathname);
 
   if (url.pathname === "/") {
-    return redirect("/revenue-report");
+    return redirect(navigation[0].route);
   }
 
   return null;
